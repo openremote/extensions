847050a90e6367cdf5ca7da0867655a1ea60acfd
Dennis Kuijs <dennis@denniskuijs.nl>
2026-07-16 17:11:48 +0200

    fix tests

--- energy/src/test/groovy/org/openremote/extension/energy/EnergyOptimisationTest.groovy
+++ energy/src/test/groovy/org/openremote/extension/energy/EnergyOptimisationTest.groovy
@@ -32,6 +32,7 @@ import static spock.util.matcher.HamcrestMatchers.closeTo
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 @Ignore
 class EnergyOptimisationTest extends Specification {
 
